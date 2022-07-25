7ceac4baec5c69ea2d8d4b5b8b3e51ad4a03ba9a
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-07-25 16:24:36 +0200

    inizio progetto da capo per risolvere problemi di node modules

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AdminController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+
+     // Non è necessaria se si mette il middleware nel web.php
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function dashboard()
+    {
+        return view('admin.dashboard');
+    }
+}
